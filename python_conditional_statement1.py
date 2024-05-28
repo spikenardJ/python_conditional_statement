a49d87266bce67cc0a6b7bfdbc2002cecfc8aadb
spikenardJ <jennifer_annette@hotmail.com>
2024-05-27 22:25:07 -0500

    first commit

---
+++ python_conditional_statement1.py
@@ -0,0 +1,12 @@
+# Question 1: Decisions at the Crossroad
+
+# Task 1: Code Correction
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
